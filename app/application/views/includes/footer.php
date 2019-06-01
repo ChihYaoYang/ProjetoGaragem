690056093772c00bc8cc4d9ff032911348023b68
YGRL <ryan47@kimo.com>
2019-05-31 22:41:42 -0300

    add footer

--- app/application/views/includes/footer.php
+++ app/application/views/includes/footer.php
@@ -1,4 +1,3 @@
-
 </div>
 <style>
     .social:hover {
@@ -34,15 +33,30 @@
         color: #f39c12;
     }
 </style>
-
-<footer class="footer mt-auto py-5">
-    <div class="container">
-        <div class="fixed-bottom text-center center-block" style="background:#64DD17;">
-            <a href="https://github.com/ChihYaoYang/ProjetoVendCar"><i id="social-git" class="fab fa-github-square fa-3x social"></i></a>
-            <a href="#"><i id="social-tw" class="fab fa-twitter-square fa-3x social"></i></a>
-            <a href="#"><i id="social-gp" class="fab fa-google-plus-square fa-3x social"></i></a>
-            <a href="#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+<!-- Footer -->
+<footer class="page-footer font-small pt-4" style="background-color:#7ed6df;">
+    <div class="container-fluid text-center text-md-left">
+        <div class="row">
+            <div class="offset-2 col-md-4">
+                <h5 class="text-uppercase text-light">VendCar</h5>
+                <p class="text-secondary">Criado pelo aluno Ryan.</p>
+            </div>
+            <div class="offset-1 col-md-5 mb-md-0 mb-3">
+                <h5 class="text-uppercase text-light">Menu</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!" class="text-secondary">Home</a></li>
+                    <li><a href="#!" class="text-secondary">Veículo</a></li>
+                    <li><a href="#!" class="text-secondary">Acessório</a></li>
+                    <li><a href="#!" class="text-secondary">Fale Conosco</a></li>
+                </ul>
+            </div>
         </div>
+    </div>
+    <div class="sticky-bottom text-center center-block" style="background:#64DD17;">
+        <a href="https://github.com/ChihYaoYang/ProjetoVendCar"><i id="social-git" class="fab fa-github-square fa-3x social"></i></a>
+        <a href="#"><i id="social-tw" class="fab fa-twitter-square fa-3x social"></i></a>
+        <a href="#"><i id="social-gp" class="fab fa-google-plus-square fa-3x social"></i></a>
+        <a href="#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
     </div>
 </footer>
 <!--Bootstrap-->
